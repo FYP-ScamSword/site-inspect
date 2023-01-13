a1c1ecc2f683d2e2439e51b56ac5f12ae85a39fe
Lee Zhi Xuan <zhixuan.lee.2020@scis.smu.edu.sg>
2023-01-13 14:48:52 +0800

    remove unused dependencies

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-
-// var corsOptions = {
-//   origin: "http://localhost:8081",
-// };
-
-// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
