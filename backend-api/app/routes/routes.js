ff1aec2f90eb534ec9678b2e28579f3f93f72733
Emmanuel Oh <emmaneugene@outlook.com>
2023-02-14 13:09:32 +0800

    chore: Pre-commit formatting

--- backend-api/app/routes/routes.js
+++ backend-api/app/routes/routes.js
@@ -1,10 +1,10 @@
 module.exports = app => {
     const controller = require("../controllers/controller.js");
-  
+
     var router = require("express").Router();
-  
+
     // post a request
     router.post("/", controller.inspectLink);
-  
+
     app.use('/api/linkinspect', router);
 };
