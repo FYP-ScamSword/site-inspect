d1827c19a9002b32ce6e631bae19690ab261ebc1
claire <claire.xw205@gmail.com>
2023-03-29 19:06:35 +0800

    fix code

--- content-inspection/src/scrape/favicon_checker.py
+++ content-inspection/src/scrape/favicon_checker.py
@@ -3,10 +3,10 @@ from typing import List, Tuple
 
 from urllib.parse import urlparse
 
-def favicon_checker(url: str) -> List[Tuple[str, List[float]]]:
+def favicon_checker(url: str) -> dict:
     parsed_url = urlparse(url)
     favicon_url = parsed_url.scheme + "://" + parsed_url.netloc + "/favicon.ico"
     similar_favicon = []
     # todo: call model
 
-    return {favicon_url:favicon_url, similar_favicon:similar_favicon}
+    return {"favicon_url":favicon_url, "similar_favicon":tuple(similar_favicon)}
