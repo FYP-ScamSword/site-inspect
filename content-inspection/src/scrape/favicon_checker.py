0cc8ac7d4fee57fa1890d0edbf5a14ee9bc50c1b
claire <claire.xw205@gmail.com>
2023-03-30 15:22:02 +0800

    integrated yun's code

--- content-inspection/src/scrape/favicon_checker.py
+++ content-inspection/src/scrape/favicon_checker.py
@@ -1,12 +1,35 @@
-from io import BytesIO
-from typing import List, Tuple
-
 from urllib.parse import urlparse
+from .image_similarity_checker import extract_features_for_k_cluster, find_similar_images
+import requests
+import io
+from PIL import Image
+
 
 def favicon_checker(url: str) -> dict:
     parsed_url = urlparse(url)
     favicon_url = parsed_url.scheme + "://" + parsed_url.netloc + "/favicon.ico"
     similar_favicon = []
-    # todo: call model
 
-    return {"favicon_url":favicon_url, "similar_favicon":tuple(similar_favicon)}
+    print(favicon_url)
+
+    try:
+        # Download the favicon.ico file
+        response = requests.get(favicon_url)
+        print(response)
+        # response.raise_for_status()
+
+        # Convert the favicon.ico file to a 32x32 PNG image
+        image = Image.open(io.BytesIO(response.content)).convert('RGB')
+        image = image.resize((32, 32))
+
+        # Save the PNG image to a file
+        image_path = parsed_url.netloc + '.png'
+        image.save(image_path)
+
+        features = extract_features_for_k_cluster(image_path)
+        similar_favicon = find_similar_images(features)
+        print(similar_favicon)
+    except (requests.exceptions.HTTPError, IOError) as e:
+        print(e)
+
+    return {"favicon_url": favicon_url, "similar_favicons": tuple(similar_favicon)}
